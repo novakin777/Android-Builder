5bd6b87f2a7f9166b01ac20a1410ed8ecbb0103a
novakin777 <72132784+novakin777@users.noreply.github.com>
2026-09-16 22:53:16 +0300

    WatchTrust: restore framework-only TrustAgent compile stub

--- WatchTrustClean/system-stubs/src/main/java/android/service/trust/TrustAgentService.java
+++ WatchTrustClean/system-stubs/src/main/java/android/service/trust/TrustAgentService.java
@@ -1,18 +1,13 @@
 package android.service.trust;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
-
 /* Compile-time stub only. It is compileOnly and MUST NOT be packaged in the APK. */
-public class TrustAgentService extends Service {
+public class TrustAgentService {
     public static final int FLAG_GRANT_TRUST_INITIATED_BY_USER = 1;
     public static final int FLAG_GRANT_TRUST_DISMISS_KEYGUARD = 2;
     public static final int FLAG_GRANT_TRUST_TEMPORARY_AND_RENEWABLE = 4;
 
-    @Override
-    public IBinder onBind(Intent intent) { return null; }
-
+    public void onCreate() {}
+    public void onDestroy() {}
     public void onTrustTimeout() {}
     public void onDeviceLocked() {}
     public void onDeviceUnlocked() {}
