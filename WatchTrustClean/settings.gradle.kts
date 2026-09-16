80161925c2f473f65d4a616d46c80ed60e6e5fc5
novakin777 <72132784+novakin777@users.noreply.github.com>
2026-09-16 17:19:12 +0300

    WatchTrust: add Gradle settings

---
+++ WatchTrustClean/settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "WatchTrustClean"
+include(":phone", ":wear", ":system-stubs")
