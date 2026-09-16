d27c23fdb7cb7c642a2f362a9438f83130da95c8
novakin777 <72132784+novakin777@users.noreply.github.com>
2026-09-16 22:48:00 +0300

    WatchTrust: make TrustAgent compile stub extend Android Service

--- WatchTrustClean/system-stubs/src/main/java/android/service/trust/TrustAgentService.java
+++ WatchTrustClean/system-stubs/src/main/java/android/service/trust/TrustAgentService.java
@@ -1,13 +1,18 @@
 package android.service.trust;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
 /* Compile-time stub only. It is compileOnly and MUST NOT be packaged in the APK. */
-public class TrustAgentService {
+public class TrustAgentService extends Service {
     public static final int FLAG_GRANT_TRUST_INITIATED_BY_USER = 1;
     public static final int FLAG_GRANT_TRUST_DISMISS_KEYGUARD = 2;
     public static final int FLAG_GRANT_TRUST_TEMPORARY_AND_RENEWABLE = 4;
 
-    public void onCreate() {}
-    public void onDestroy() {}
+    @Override
+    public IBinder onBind(Intent intent) { return null; }
+
     public void onTrustTimeout() {}
     public void onDeviceLocked() {}
     public void onDeviceUnlocked() {}
