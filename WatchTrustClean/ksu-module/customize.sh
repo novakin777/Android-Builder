2e0ab83e93d5d4d2446d3cfe02254bacdac6cf7b
novakin777 <72132784+novakin777@users.noreply.github.com>
2026-09-17 15:03:19 +0300

    WatchTrust: install phone APK at fresh WatchTrust107 path

--- WatchTrustClean/ksu-module/customize.sh
+++ WatchTrustClean/ksu-module/customize.sh
@@ -1,13 +1,14 @@
 #!/system/bin/sh
 
 ui_print "*******************************"
-ui_print "   WatchTrust 1.6-wear-diag"
+ui_print "      WatchTrust 1.7-stable"
 ui_print "*******************************"
 
-DEST_DIR="$MODPATH/system/priv-app/WatchTrust106"
+DEST_DIR="$MODPATH/system/priv-app/WatchTrust107"
 DEST_APK="$DEST_DIR/WatchTrust.apk"
 OLD_DIR1="$MODPATH/system/priv-app/WatchTrust"
 OLD_DIR2="$MODPATH/system/priv-app/WatchTrust105"
+OLD_DIR3="$MODPATH/system/priv-app/WatchTrust106"
 PAYLOAD="$MODPATH/watchtrust-phone.apk"
 XML="$MODPATH/system/etc/permissions/privapp-permissions-watchtrust.xml"
 
@@ -19,6 +20,7 @@ fi
 
 rm -rf "$OLD_DIR1"
 rm -rf "$OLD_DIR2"
+rm -rf "$OLD_DIR3"
 rm -rf "$DEST_DIR"
 mkdir -p "$DEST_DIR"
 
@@ -31,5 +33,5 @@ set_perm_recursive "$DEST_DIR" 0 0 0755 0644
 set_perm "$DEST_APK" 0 0 0644
 set_perm "$XML" 0 0 0644
 
-ui_print "- Installed: /system/priv-app/WatchTrust106/WatchTrust.apk"
+ui_print "- Installed: /system/priv-app/WatchTrust107/WatchTrust.apk"
 ui_print "- Reboot required"
