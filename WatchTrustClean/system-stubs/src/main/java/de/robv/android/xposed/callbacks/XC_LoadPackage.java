e8b6943b136a743829f5bdb14c38ac00256ea8a2
novakin777 <72132784+novakin777@users.noreply.github.com>
2026-09-17 13:14:20 +0300

    WatchTrust: fix legacy Xposed stub compilation

--- WatchTrustClean/system-stubs/src/main/java/de/robv/android/xposed/callbacks/XC_LoadPackage.java
+++ WatchTrustClean/system-stubs/src/main/java/de/robv/android/xposed/callbacks/XC_LoadPackage.java
@@ -1,7 +1,5 @@
 package de.robv.android.xposed.callbacks;
 
-import android.content.pm.ApplicationInfo;
-
 public final class XC_LoadPackage {
     private XC_LoadPackage() {}
 
@@ -9,7 +7,6 @@ public final class XC_LoadPackage {
         public String packageName;
         public String processName;
         public ClassLoader classLoader;
-        public ApplicationInfo appInfo;
         public boolean isFirstApplication;
     }
 }
